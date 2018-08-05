3bae0616b06517ef205aecfca0a9e6e1bbc4f2ee
Dawid24 <paciorekdawidonly@gmail.com>
2018-08-05 14:55:26 +0200

    version2

--- src/GUI.java
+++ src/GUI.java
@@ -11,8 +11,19 @@ public class GUI {
         String model = scanner.nextLine();
         System.out.println("Cena: ");
         String price = scanner.nextLine();
+        try {
+            validatePrice(price);
+            Car car = new Car(brand, model, Double.parseDouble(price));
+            Repository.addCar(car);
+        } catch (ParsePriceException e) {
+            System.out.println(e.getMessage());
+        }
 
-        Car car = new Car(brand, model, Double.parseDouble(price));
-        Repository.addCar(car);
+    }
+    private static void validatePrice(String price) throws ParsePriceException {
+        String pattern = "[0-9]+([\\.]{1}[0-9]{2})?";
+        if (!price.matches(pattern)) {
+            throw new ParsePriceException();
+        }
     }
 }
---
+++ src/ParsePriceException.java
@@ -0,0 +1,7 @@
+public class ParsePriceException extends Exception {
+
+    @Override
+    public String getMessage() {
+        return "Podana cena jest nieprawidłowa!";
+    }
+}
